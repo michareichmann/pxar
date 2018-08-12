6f81cfb7ac036b992e099a273a2765a771f6703e
michareichmann <micha.reichmann@gmail.com>
2018-08-12 09:26:30 +0200

    small fix

--- python/CLIX.py
+++ python/CLIX.py
@@ -1253,7 +1253,7 @@ class PxarCoreCmd(cmd.Cmd):
                 elif eff > 0:
                     print '{c}{eff:1.1f}{e}'.format(eff=eff, c=RED, e=ENDC),
                 else:
-                    print ' x ',
+                    print ' - ',
             print
         if not good:
             'Did not find any good timing...'
