9fdede08bada4a37013b393fd7a7d5cc83a611c4
michareichmann <remichae@phys.ethz.ch>
2019-10-07 11:17:59 +0200

    add offset to analogue decoding

--- python/iCLIX.py
+++ python/iCLIX.py
@@ -111,24 +111,22 @@ class CLIX:
         return (num & 0x0ffc) == 0x7f8
 
     @staticmethod
-    def translate_level(value, level1, level_s):
-        level = int((value + level1 + level_s) / level1)
+    def translate_level(value, level1, level_s, offset=0):
+        level = int((value + level1 + level_s + offset) / level1)
         return 5 if level >= 5 else level
 
-    def decode_analogue(self, value):
+    def decode_analogue(self, value, offset=0):
         black = value[1]
         level1 = (black - value[0]) / 4
         level_s = level1 / 2
         ph = value[-1]
 
-        c1 = self.translate_level(value[3], level1, level_s)
-        c0 = self.translate_level(value[4], level1, level_s)
-        c = c1 * 6 + c0
+        c1 = self.translate_level(value[3], level1, level_s, offset)
+        c0 = self.translate_level(value[4], level1, level_s, offset)
 
-        r2 = self.translate_level(value[5], level1, level_s)
-        r1 = self.translate_level(value[6], level1, level_s)
-        r0 = self.translate_level(value[7], level1, level_s)
-        r = (r2 * 6 + r1) * 6 + r0
+        r2 = self.translate_level(value[5], level1, level_s, offset)
+        r1 = self.translate_level(value[6], level1, level_s, offset)
+        r0 = self.translate_level(value[7], level1, level_s, offset)
 
         column, row = calculate_col_row(c1, c0, r2, r1, r0)
 
@@ -137,6 +135,9 @@ class CLIX:
         print '{:2d}, {}\n'.format(level_s, ' '.join(['{:3d}'.format(i) for i in [c1, c0, r2, r1, r0]]))
         print '\n===== [{c}, {r}, {p}] ====='.format(c=column, r=row, p=ph)
 
+    def set_offset(self, value):
+        self.api.setDecodingOffset(value)
+        print 'set analogue decoding offset to: {}'.format(value)
 
     @staticmethod
     def decode_digital(value):
